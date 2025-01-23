bee904d29ac8c755c113111a7f45581e884d2f4f
Kim Sand <kimsand@gmail.com>
2025-01-23 14:19:02 +0100

    The librarian project is now UI only, with the patch logic in a separate package.

--- ThirdWaveLibrarian/BanksView.swift
+++ ThirdWaveLibrarian/BanksView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ThirdWaveLibrary
 
 struct BanksView: View {
     @Binding var banks: Banks
--- ThirdWaveLibrarian/ThirdWaveLibrarianApp.swift
+++ ThirdWaveLibrarian/ThirdWaveLibrarianApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ThirdWaveLibrary
 
 @main
 struct ThirdWaveLibrarianApp: App {
